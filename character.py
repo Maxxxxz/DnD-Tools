b041a3bf8439c7e4b6a6549c2ecc4c3a98afddc4
Michael Cooper <michaelcooper99@hotmail.com>
2019-07-10 16:59:46 -0400

    added more methods

--- character.py
+++ character.py
@@ -6,15 +6,37 @@ class Character():
         self.Name = "undefined"
         self.Class = "undefined"
         self.Stats = [0, 0, 0, 0, 0, 0]
+        self.Money = [0, 0, 0, 0] #pp, gp, sp, cp
+        self.MaxHP = "0"
+        self.HP = "0"
+        self.Skills = ["undefined"]
 
-    def setName(self, Name):
-        self.Name = Name
+
+    def printAll(self):
+        print(vars(self))
+
+    def setName(self, newName):
+        self.Name = newName
+
+    def setClass(self, newClass):
+        self.Class = newClass
 
     def setStats(self, newStats):
         self.Stats = newStats
 
+    def setMoney(self, newMoney):
+        self.Money = newMoney
+
+    def setMaxHP(self, newMaxHP):
+        self.MaxHP = newMaxHP
+
+    def setHP(self, newHP):
+        self.HP = newHP
+
+    def setSkills(self, newSkills):
+        self.Skills = newSkills
+
 if __name__ == "__main__":
     char = Character()
-    print(char.Name)
     char.setStats([1, 2, 3, 4, 5, 6])
-    print(char.Stats)
+    char.printAll()
